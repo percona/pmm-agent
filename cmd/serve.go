fc61914e5f5241b00c0f1d0bfe75af352067cf7c
Kamil Dziedzic <kamil@klecza.pl>
2018-10-12 12:36:46 +0200

    Update serve.go

--- cmd/serve.go
+++ cmd/serve.go
@@ -16,13 +16,7 @@ import (
 // serveCmd represents the serve command
 var serveCmd = &cobra.Command{
 	Use:   "serve",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Serve accepts incomming connections and serves responses.",
 	RunE: func(cmd *cobra.Command, args []string) error {
 		errs := &errs.Safe{}
 		wg := &sync.WaitGroup{}
