cc13241af41b3ca20ca3f211a375aa2c741dac0b
Andrii Skomorokhov <andrii.skomorokhov@percona.com>
2019-04-17 12:04:00 +0300

    PMM-3844 Added qan slow log parser.

--- agents/builtin/mysql/slowlog/slowlog.go
+++ agents/builtin/mysql/slowlog/slowlog.go
@@ -21,9 +21,16 @@ import (
 	"context"
 	"database/sql"
 	"fmt"
+	"os"
+	"path/filepath"
 	"time"
 
 	_ "github.com/go-sql-driver/mysql" // register SQL driver
+	"github.com/percona/go-mysql/event"
+	"github.com/percona/go-mysql/log"
+	slowlog "github.com/percona/go-mysql/log"
+	parser "github.com/percona/go-mysql/log/slow"
+	"github.com/percona/go-mysql/query"
 	"github.com/percona/pmm/api/inventorypb"
 	"github.com/percona/pmm/api/qanpb"
 	"github.com/sirupsen/logrus"
@@ -33,13 +40,18 @@ import (
 
 const (
 	queryInterval = time.Minute
+	// TODO: get some
+	agentUUID = "dc889ca7be92a66f0a00f616f69ffa7b"
 )
 
 // SlowLog extracts performance data from MySQL slow log.
 type SlowLog struct {
-	db      *reform.DB
-	l       *logrus.Entry
-	changes chan Change
+	db               *reform.DB
+	l                *logrus.Entry
+	slowQueryLog     uint8
+	slowQueryLogFile string
+	changes          chan Change
+	aggregator       *event.Aggregator
 }
 
 // Params represent Agent parameters.
@@ -63,7 +75,6 @@ func New(params *Params, l *logrus.Entry) (*SlowLog, error) {
 	sqlDB.SetMaxOpenConns(1)
 	sqlDB.SetConnMaxLifetime(0)
 	db := reform.NewDB(sqlDB, mysql.Dialect, reform.NewPrintfLogger(l.Tracef))
-
 	return newMySQL(db, l), nil
 }
 
@@ -77,11 +88,43 @@ func newMySQL(db *reform.DB, l *logrus.Entry) *SlowLog {
 
 // Run extracts performance data and sends it to the channel until ctx is canceled.
 func (m *SlowLog) Run(ctx context.Context) {
+	fmt.Println("\n\n ---- MY SLOW LOG RUN ---- \n\n")
 	defer func() {
 		m.db.DBInterface().(*sql.DB).Close() //nolint:errcheck
 		m.changes <- Change{Status: inventorypb.AgentStatus_DONE}
 		close(m.changes)
 	}()
+
+	row := m.db.QueryRow("SELECT @@slow_query_log")
+	if err := row.Scan(&m.slowQueryLog); err != nil {
+		m.l.Errorf("cannon select @@slow_query_log:%v", err)
+	}
+	if m.slowQueryLog != 1 {
+		m.l.Errorf("cannot parse slowlog: @@slow_query_log is off: %v", m.slowQueryLog)
+	}
+
+	row = m.db.QueryRow("SELECT @@slow_query_log_file")
+	if err := row.Scan(&m.slowQueryLogFile); err != nil {
+		m.l.Errorf("cannon select @@slow_query_log_file:%v", err)
+	}
+	if m.slowQueryLogFile == "" {
+		m.l.Errorf("cannot parse slowlog: @@slow_query_log_file is empty: %v", m.slowQueryLogFile)
+	}
+
+	m.l.Infof("@@slow_query_log: %v; @@slow_query_log_file: %v.", m.slowQueryLog, m.slowQueryLogFile)
+
+	file, err := os.Open(filepath.Clean(m.slowQueryLogFile))
+	if err != nil {
+		m.l.Errorf("cannot open slowlog (%s): %v", m.slowQueryLogFile, err)
+	}
+	p := parser.NewSlowLogParser(file, slowlog.Options{})
+	go func() {
+		err = p.Start()
+		if err != nil {
+			m.l.Errorf("cannot start parser: %v", err)
+		}
+	}()
+	logEvent := p.EventChan()
 
 	var running bool
 	m.changes <- Change{Status: inventorypb.AgentStatus_STARTING}
@@ -96,20 +139,16 @@ func (m *SlowLog) Run(ctx context.Context) {
 		select {
 		case <-ctx.Done():
 			m.changes <- Change{Status: inventorypb.AgentStatus_STOPPING}
+			p.Stop()
 			m.l.Infof("Context canceled.")
 			return
 
-		case <-t.C:
+		case logEntry := <-logEvent:
 			if !running {
 				m.changes <- Change{Status: inventorypb.AgentStatus_STARTING}
 			}
 
-			buckets, err := m.getNewBuckets()
-
-			start = time.Now().Truncate(0) // strip monotoning clock reading
-			wait = start.Truncate(queryInterval).Add(queryInterval).Sub(start)
-			m.l.Debugf("Scheduling next collection in %s at %s.", wait, start.Add(wait))
-			t.Reset(wait)
+			buckets, err := m.getNewBuckets(logEntry)
 
 			if err != nil {
 				m.l.Error(err)
@@ -123,23 +162,289 @@ func (m *SlowLog) Run(ctx context.Context) {
 				m.changes <- Change{Status: inventorypb.AgentStatus_RUNNING}
 			}
 
+			if len(buckets) == 0 {
+				continue
+			}
+
 			m.changes <- Change{Request: &qanpb.CollectRequest{MetricsBucket: buckets}}
+
 		}
 	}
 }
 
-func (m *SlowLog) getNewBuckets() ([]*qanpb.MetricsBucket, error) {
-	return makeBuckets()
+func (m *SlowLog) getNewBuckets(e *log.Event) ([]*qanpb.MetricsBucket, error) {
+	if m.aggregator == nil {
+		m.aggregator = event.NewAggregator(true, 0, 1)
+	}
+	fingerprint := query.Fingerprint(e.Query)
+	digest := query.Id(fingerprint)
+
+	m.aggregator.AddEvent(e, digest, e.User, e.Host, e.Db, e.Server, fingerprint)
+
+	// TODO: here we need to juggle with prev/next events
+	if e.Ts.Second() < 50 {
+		return []*qanpb.MetricsBucket{}, nil
+	}
+
+	res := m.aggregator.Finalize()
+	m.aggregator = nil
+
+	return makeBuckets(res, e.Ts)
 }
 
 // makeBuckets XXX.
 //
 // makeBuckets is a pure function for easier testing.
-func makeBuckets() ([]*qanpb.MetricsBucket, error) {
-	return nil, fmt.Errorf("not implemented yet")
+func makeBuckets(res event.Result, ts time.Time) ([]*qanpb.MetricsBucket, error) {
+	buckets := []*qanpb.MetricsBucket{}
+	for _, v := range res.Class {
+
+		mb := &qanpb.MetricsBucket{
+			Queryid:             v.Id,
+			Fingerprint:         v.Fingerprint,
+			DDatabase:           "",
+			DSchema:             v.Db,
+			DUsername:           v.User,
+			DClientHost:         v.Host,
+			DServer:             v.Server,
+			Labels:              listsToMap(v.LabelsKey, v.LabelsValue),
+			AgentUuid:           agentUUID,
+			MetricsSource:       qanpb.MetricsSource_MYSQL_SLOWLOG,
+			PeriodStartUnixSecs: uint32(ts.Truncate(1 * time.Minute).Unix()),
+			PeriodLengthSecs:    uint32(60),
+			Example:             v.Example.Query,
+			ExampleFormat:       1,
+			ExampleType:         1,
+			NumQueries:          float32(v.TotalQueries),
+		}
+
+		// If key has suffix _time or _wait than field is TimeMetrics.
+		// For Boolean metrics exists only Sum.
+		// https://www.percona.com/doc/percona-server/5.7/diagnostics/slow_extended.html
+		// TimeMetrics: query_time, lock_time, rows_sent, innodb_io_r_wait, innodb_rec_lock_wait, innodb_queue_wait.
+		// NumberMetrics: rows_examined, rows_affected, rows_read, merge_passes, innodb_io_r_ops, innodb_io_r_bytes,
+		// innodb_pages_distinct, query_length, bytes_sent, tmp_tables, tmp_disk_tables, tmp_table_sizes.
+		// BooleanMetrics: qc_hit, full_scan, full_join, tmp_table, tmp_table_on_disk, filesort, filesort_on_disk,
+		// select_full_range_join, select_range, select_range_check, sort_range, sort_rows, sort_scan,
+		// no_index_used, no_good_index_used.
+
+		// query_time - Query_time
+		if m, ok := v.Metrics.TimeMetrics["Query_time"]; ok {
+			mb.MQueryTimeSum = float32(m.Sum)
+			mb.MQueryTimeMax = float32(*m.Max)
+			mb.MQueryTimeMin = float32(*m.Min)
+			mb.MQueryTimeP99 = float32(*m.P99)
+		}
+		// lock_time - Lock_time
+		if m, ok := v.Metrics.TimeMetrics["Lock_time"]; ok {
+			mb.MLockTimeSum = float32(m.Sum)
+			mb.MLockTimeMax = float32(*m.Max)
+			mb.MLockTimeMin = float32(*m.Min)
+			mb.MLockTimeP99 = float32(*m.P99)
+		}
+		// rows_sent - Rows_sent
+		if m, ok := v.Metrics.NumberMetrics["Rows_sent"]; ok {
+			mb.MRowsSentSum = float32(m.Sum)
+			mb.MRowsSentMax = float32(*m.Max)
+			mb.MRowsSentMin = float32(*m.Min)
+			mb.MRowsSentP99 = float32(*m.P99)
+		}
+		// rows_examined - Rows_examined
+		if m, ok := v.Metrics.NumberMetrics["Rows_examined"]; ok {
+			mb.MRowsExaminedSum = float32(m.Sum)
+			mb.MRowsExaminedMax = float32(*m.Max)
+			mb.MRowsExaminedMin = float32(*m.Min)
+			mb.MRowsExaminedP99 = float32(*m.P99)
+		}
+		// rows_affected - Rows_affected
+		if m, ok := v.Metrics.NumberMetrics["Rows_affected"]; ok {
+			mb.MRowsAffectedSum = float32(m.Sum)
+			mb.MRowsAffectedMax = float32(*m.Max)
+			mb.MRowsAffectedMin = float32(*m.Min)
+			mb.MRowsAffectedP99 = float32(*m.P99)
+		}
+		// rows_read - Rows_read
+		if m, ok := v.Metrics.NumberMetrics["Rows_read"]; ok {
+			mb.MRowsReadSum = float32(m.Sum)
+			mb.MRowsReadMax = float32(*m.Max)
+			mb.MRowsReadMin = float32(*m.Min)
+			mb.MRowsReadP99 = float32(*m.P99)
+		}
+		// merge_passes - Merge_passes
+		if m, ok := v.Metrics.NumberMetrics["Merge_passes"]; ok {
+			mb.MMergePassesSum = float32(m.Sum)
+			mb.MMergePassesMax = float32(*m.Max)
+			mb.MMergePassesMin = float32(*m.Min)
+			mb.MMergePassesP99 = float32(*m.P99)
+		}
+		// innodb_io_r_ops - InnoDB_IO_r_ops
+		if m, ok := v.Metrics.NumberMetrics["InnoDB_IO_r_ops"]; ok {
+			mb.MInnodbIoROpsSum = float32(m.Sum)
+			mb.MInnodbIoROpsMax = float32(*m.Max)
+			mb.MInnodbIoROpsMin = float32(*m.Min)
+			mb.MInnodbIoROpsP99 = float32(*m.P99)
+		}
+		// innodb_io_r_bytes - InnoDB_IO_r_bytes
+		if m, ok := v.Metrics.NumberMetrics["InnoDB_IO_r_bytes"]; ok {
+			mb.MInnodbIoRBytesSum = float32(m.Sum)
+			mb.MInnodbIoRBytesMax = float32(*m.Max)
+			mb.MInnodbIoRBytesMin = float32(*m.Min)
+			mb.MInnodbIoRBytesP99 = float32(*m.P99)
+		}
+		// innodb_io_r_wait - InnoDB_IO_r_wait
+		if m, ok := v.Metrics.TimeMetrics["InnoDB_IO_r_wait"]; ok {
+			mb.MInnodbIoRWaitSum = float32(m.Sum)
+			mb.MInnodbIoRWaitMax = float32(*m.Max)
+			mb.MInnodbIoRWaitMin = float32(*m.Min)
+			mb.MInnodbIoRWaitP99 = float32(*m.P99)
+		}
+		// innodb_rec_lock_wait - InnoDB_rec_lock_wait
+		if m, ok := v.Metrics.TimeMetrics["InnoDB_rec_lock_wait"]; ok {
+			mb.MInnodbRecLockWaitSum = float32(m.Sum)
+			mb.MInnodbRecLockWaitMax = float32(*m.Max)
+			mb.MInnodbRecLockWaitMin = float32(*m.Min)
+			mb.MInnodbRecLockWaitP99 = float32(*m.P99)
+		}
+		// innodb_queue_wait - InnoDB_queue_wait
+		if m, ok := v.Metrics.TimeMetrics["InnoDB_queue_wait"]; ok {
+			mb.MInnodbQueueWaitSum = float32(m.Sum)
+			mb.MInnodbQueueWaitMax = float32(*m.Max)
+			mb.MInnodbQueueWaitMin = float32(*m.Min)
+			mb.MInnodbQueueWaitP99 = float32(*m.P99)
+		}
+		// innodb_pages_distinct - InnoDB_pages_distinct
+		if m, ok := v.Metrics.NumberMetrics["InnoDB_pages_distinct"]; ok {
+			mb.MInnodbPagesDistinctSum = float32(m.Sum)
+			mb.MInnodbPagesDistinctMax = float32(*m.Max)
+			mb.MInnodbPagesDistinctMin = float32(*m.Min)
+			mb.MInnodbPagesDistinctP99 = float32(*m.P99)
+		}
+		// query_length - Query_length
+		if m, ok := v.Metrics.NumberMetrics["Query_length"]; ok {
+			mb.MQueryLengthSum = float32(m.Sum)
+			mb.MQueryLengthMax = float32(*m.Max)
+			mb.MQueryLengthMin = float32(*m.Min)
+			mb.MQueryLengthP99 = float32(*m.P99)
+		}
+		// bytes_sent - Bytes_sent
+		if m, ok := v.Metrics.NumberMetrics["Bytes_sent"]; ok {
+			mb.MBytesSentSum = float32(m.Sum)
+			mb.MBytesSentMax = float32(*m.Max)
+			mb.MBytesSentMin = float32(*m.Min)
+			mb.MBytesSentP99 = float32(*m.P99)
+		}
+		// tmp_tables - Tmp_tables
+		if m, ok := v.Metrics.NumberMetrics["Tmp_tables"]; ok {
+			mb.MTmpTablesSum = float32(m.Sum)
+			mb.MTmpTablesMax = float32(*m.Max)
+			mb.MTmpTablesMin = float32(*m.Min)
+			mb.MTmpTablesP99 = float32(*m.P99)
+		}
+		// tmp_disk_tables - Tmp_disk_tables
+		if m, ok := v.Metrics.NumberMetrics["Tmp_disk_tables"]; ok {
+			mb.MTmpDiskTablesSum = float32(m.Sum)
+			mb.MTmpDiskTablesMax = float32(*m.Max)
+			mb.MTmpDiskTablesMin = float32(*m.Min)
+			mb.MTmpDiskTablesP99 = float32(*m.P99)
+		}
+		// tmp_table_sizes - Tmp_table_sizes
+		if m, ok := v.Metrics.NumberMetrics["Tmp_table_sizes"]; ok {
+			mb.MTmpTableSizesSum = float32(m.Sum)
+			mb.MTmpTableSizesMax = float32(*m.Max)
+			mb.MTmpTableSizesMin = float32(*m.Min)
+			mb.MTmpTableSizesP99 = float32(*m.P99)
+		}
+		// qc_hit - QC_Hit
+		if m, ok := v.Metrics.BoolMetrics["QC_Hit"]; ok {
+			mb.MQcHitCnt = float32(m.Cnt)
+			mb.MQcHitSum = float32(m.Sum)
+		}
+		// full_scan - Full_scan
+		if m, ok := v.Metrics.BoolMetrics["Full_scan"]; ok {
+			mb.MFullScanCnt = float32(m.Cnt)
+			mb.MFullScanSum = float32(m.Sum)
+		}
+		// full_join - Full_join
+		if m, ok := v.Metrics.BoolMetrics["Full_join"]; ok {
+			mb.MFullJoinCnt = float32(m.Cnt)
+			mb.MFullJoinSum = float32(m.Sum)
+		}
+		// tmp_table - Tmp_table
+		if m, ok := v.Metrics.BoolMetrics["Tmp_table"]; ok {
+			mb.MTmpTableCnt = float32(m.Cnt)
+			mb.MTmpTableSum = float32(m.Sum)
+		}
+		// tmp_table_on_disk - Tmp_table_on_disk
+		if m, ok := v.Metrics.BoolMetrics["Tmp_table_on_disk"]; ok {
+			mb.MTmpTableOnDiskCnt = float32(m.Cnt)
+			mb.MTmpTableOnDiskSum = float32(m.Sum)
+		}
+		// filesort - Filesort
+		if m, ok := v.Metrics.BoolMetrics["Filesort"]; ok {
+			mb.MFilesortCnt = float32(m.Cnt)
+			mb.MFilesortSum = float32(m.Sum)
+		}
+		// filesort_on_disk - Filesort_on_disk
+		if m, ok := v.Metrics.BoolMetrics["Filesort_on_disk"]; ok {
+			mb.MFilesortOnDiskCnt = float32(m.Cnt)
+			mb.MFilesortOnDiskSum = float32(m.Sum)
+		}
+		// select_full_range_join - Select_full_range_join
+		if m, ok := v.Metrics.BoolMetrics["Select_full_range_join"]; ok {
+			mb.MSelectFullRangeJoinCnt = float32(m.Cnt)
+			mb.MSelectFullRangeJoinSum = float32(m.Sum)
+		}
+		// select_range - Select_range
+		if m, ok := v.Metrics.BoolMetrics["Select_range"]; ok {
+			mb.MSelectRangeCnt = float32(m.Cnt)
+			mb.MSelectRangeSum = float32(m.Sum)
+		}
+		// select_range_check - Select_range_check
+		if m, ok := v.Metrics.BoolMetrics["Select_range_check"]; ok {
+			mb.MSelectRangeCheckCnt = float32(m.Cnt)
+			mb.MSelectRangeCheckSum = float32(m.Sum)
+		}
+		// sort_range - Sort_range
+		if m, ok := v.Metrics.BoolMetrics["Sort_range"]; ok {
+			mb.MSortRangeCnt = float32(m.Cnt)
+			mb.MSortRangeSum = float32(m.Sum)
+		}
+		// sort_rows - Sort_rows
+		if m, ok := v.Metrics.BoolMetrics["Sort_rows"]; ok {
+			mb.MSortRowsCnt = float32(m.Cnt)
+			mb.MSortRowsSum = float32(m.Sum)
+		}
+		// sort_scan - Sort_scan
+		if m, ok := v.Metrics.BoolMetrics["Sort_scan"]; ok {
+			mb.MSortScanCnt = float32(m.Cnt)
+			mb.MSortScanSum = float32(m.Sum)
+		}
+		// no_index_used - No_index_used
+		if m, ok := v.Metrics.BoolMetrics["No_index_used"]; ok {
+			mb.MNoIndexUsedCnt = float32(m.Cnt)
+			mb.MNoIndexUsedSum = float32(m.Sum)
+		}
+		// no_good_index_used - No_good_index_used
+		if m, ok := v.Metrics.BoolMetrics["No_good_index_used"]; ok {
+			mb.MNoGoodIndexUsedCnt = float32(m.Cnt)
+			mb.MNoGoodIndexUsedSum = float32(m.Sum)
+		}
+
+		buckets = append(buckets, mb)
+	}
+
+	return buckets, nil
 }
 
 // Changes returns channel that should be read until it is closed.
 func (m *SlowLog) Changes() <-chan Change {
 	return m.changes
+}
+
+func listsToMap(k, v []string) map[string]string {
+	m := map[string]string{}
+	for i, e := range k {
+		m[e] = v[i]
+	}
+	return m
 }
