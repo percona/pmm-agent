2a893ab85edaba29b78fa0921f880de47d367044
Anton Kucherov <anton.kucherov@percona.com>
2019-05-21 16:37:40 +0300

    PMM-3832 Working on base implementation.

--- actions/mysql_explain_action.go
+++ actions/mysql_explain_action.go
@@ -18,6 +18,11 @@ package actions
 
 import (
 	"context"
+	"database/sql"
+	"encoding/json"
+	"fmt"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 type mysqlExplainOutputFormat string
@@ -57,5 +62,76 @@ func (p *mysqlExplainAction) Type() string {
 
 // Run starts an Action. This method is blocking.
 func (p *mysqlExplainAction) Run(ctx context.Context) ([]byte, error) {
-	return nil, nil
+	db, err := sql.Open("mysql", p.dsn)
+	if err != nil {
+		return nil, err
+	}
+	defer db.Close()
+
+	var bytes []byte
+
+	switch p.format {
+	case ExplainFormatDefault:
+		statement := fmt.Sprintf("EXPLAIN %s", p.query)
+		out := explainRow{}
+		r := db.QueryRowContext(ctx, statement)
+
+		err = r.Scan(
+			&out.Id,
+			&out.SelectType,
+			&out.Table,
+			&out.Partitions,
+			&out.Type,
+			&out.PossibleKeys,
+			&out.Key,
+			&out.KeyLen,
+			&out.Ref,
+			&out.Rows,
+			&out.Filtered,
+			&out.Extra,
+		)
+		if err != nil {
+			return nil, err
+		}
+
+		bytes, err = json.Marshal(out)
+		if err != nil {
+			return nil, err
+		}
+	case ExplainFormatJSON:
+		statement := fmt.Sprintf("EXPLAIN FORMAT=JSON %s", p.query)
+		out := explainRowJson{}
+
+		r := db.QueryRowContext(ctx, statement)
+		err = r.Scan(&out.Explain)
+		if err != nil {
+			return nil, err
+		}
+
+		bytes, err = json.Marshal(out)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	return bytes, nil
+}
+
+type explainRow struct {
+	Id           sql.NullInt64
+	SelectType   sql.NullString
+	Table        sql.NullString
+	Partitions   sql.NullString
+	Type         sql.NullString
+	PossibleKeys sql.NullString
+	Key          sql.NullString
+	KeyLen       sql.NullString
+	Ref          sql.NullString
+	Rows         sql.NullInt64
+	Filtered     sql.NullFloat64
+	Extra        sql.NullString
+}
+
+type explainRowJson struct {
+	Explain sql.NullString
 }
---
+++ actions/mysql_explain_action_test.go
@@ -0,0 +1,55 @@
+// pmm-agent
+// Copyright (C) 2018 Percona LLC
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+package actions
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestMysqlExplainActionRun(t *testing.T) {
+	id := "/action_id/6a479303-5081-46d0-baa0-87d6248c987b"
+	dsn := "pmm-agent:pmm-agent-password@tcp(127.0.0.1:3306)/information_schema"
+	q := "SELECT * FROM information_schema.GLOBAL_STATUS"
+
+	exp := NewMySQLExplainAction(id, dsn, q, ExplainFormatDefault)
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	out, err := exp.Run(ctx)
+	require.NoError(t, err)
+	assert.NotEmpty(t, out)
+}
+
+func TestMysqlExplainActionRunJson(t *testing.T) {
+	id := "/action_id/6a479303-5081-46d0-baa0-87d6248c987b"
+	dsn := "pmm-agent:pmm-agent-password@tcp(127.0.0.1:3306)/information_schema"
+	q := "SELECT * FROM information_schema.GLOBAL_STATUS"
+
+	exp := NewMySQLExplainAction(id, dsn, q, ExplainFormatJSON)
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	out, err := exp.Run(ctx)
+	require.NoError(t, err)
+	assert.NotEmpty(t, out)
+}
