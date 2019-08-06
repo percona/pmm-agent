7fd1382c74fca20d48ab86bdad725b34ce70cafe
Nurlan Moldomurov <nurlan.moldomurov@percona.com>
2019-08-07 02:49:38 +0300

    PMM-4181 SHOW CREATE TABLE Action

---
+++ actions/postgresql_show_create_table_action_test.go
@@ -0,0 +1,104 @@
+// pmm-agent
+// Copyright (C) 2018 Percona LLC
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+package actions
+
+import (
+	"context"
+	"strings"
+	"testing"
+	"time"
+
+	"github.com/percona/pmm/api/agentpb"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
+	"github.com/percona/pmm-agent/utils/tests"
+)
+
+func TestPostgreSQLShowCreateTable(t *testing.T) {
+	t.Parallel()
+
+	dsn := tests.GetTestPostgreSQLDSN(t)
+	db := tests.OpenTestPostgreSQL(t)
+	defer db.Close() //nolint:errcheck
+	//postgreSQLVendor := tests.PostgreSQLVersion(t, db)
+
+	t.Run("Default", func(t *testing.T) {
+		params := &agentpb.StartActionRequest_PostgreSQLShowCreateTableParams{
+			Dsn:   dsn,
+			Table: "country",
+		}
+		a := NewPostgreSQLShowCreateTableAction("", params)
+		ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+		defer cancel()
+
+		b, err := a.Run(ctx)
+		require.NoError(t, err)
+
+		var expected string
+			// `DEFAULT '0'` for Population
+			expected = strings.TrimSpace(`
+                                         Table "public.country"
+Column         |Type          |Collation |Nullable |Default |Storage  |Stats target |Description
+code           |character(3)  |          |not null |        |extended |             |
+name           |text          |          |not null |        |extended |             |
+continent      |text          |          |not null |        |extended |             |
+region         |text          |          |not null |        |extended |             |
+surfacearea    |real          |          |not null |        |plain    |             |
+indepyear      |smallint      |          |         |        |plain    |             |
+population     |integer       |          |not null |        |plain    |             |
+lifeexpectancy |real          |          |         |        |plain    |             |
+gnp            |numeric(10,2) |          |         |        |main     |             |
+gnpold         |numeric(10,2) |          |         |        |main     |             |
+localname      |text          |          |not null |        |extended |             |
+governmentform |text          |          |not null |        |extended |             |
+headofstate    |text          |          |         |        |extended |             |
+capital        |integer       |          |         |        |plain    |             |
+code2          |character(2)  |          |not null |        |extended |             |
+Indexes:
+	"country_pkey" PRIMARY KEY, btree (code)
+Check constraints:
+	"country_continent_check" CHECK (continent = 'Asia'::text OR continent = 'Europe'::text OR continent = 'North America'::text OR continent = 'Africa'::text OR continent = 'Oceania'::text OR continent = 'Antarctica'::text OR continent = 'South America'::text)
+Foreign-key constraints:
+	"country_capital_fkey" FOREIGN KEY (capital) REFERENCES city(id)
+Referenced by:
+	TABLE "countrylanguage" CONSTRAINT "countrylanguage_countrycode_fkey" FOREIGN KEY (countrycode) REFERENCES country(code)
+
+			`)
+
+		assert.Equal(t, expected, string(b))
+	})
+	
+	t.Run("LittleBobbyTables", func(t *testing.T) {
+		params := &agentpb.StartActionRequest_PostgreSQLShowCreateTableParams{
+			Dsn:   dsn,
+			Table: `city; DROP TABLE city; --`,
+		}
+		a := NewPostgreSQLShowCreateTableAction("", params)
+		ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+		defer cancel()
+
+		_, err := a.Run(ctx)
+		expected := "Table not found: sql: no rows in result set"
+		assert.EqualError(t, err, expected)
+
+		var count int
+		err = db.QueryRow("SELECT COUNT(*) FROM city").Scan(&count)
+		require.NoError(t, err)
+		assert.Equal(t, 4079, count)
+	})
+}
---
+++ actions/postgresql_show_index_action.go
@@ -0,0 +1,72 @@
+// pmm-agent
+// Copyright (C) 2018 Percona LLC
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+package actions
+
+import (
+	"context"
+	"database/sql"
+
+	_ "github.com/lib/pq" // register SQL driver
+	"github.com/percona/pmm/api/agentpb"
+)
+
+type postgresqlShowIndexAction struct {
+	id     string
+	params *agentpb.StartActionRequest_PostgreSQLShowIndexParams
+}
+
+// NewPostgreSQLShowIndexAction creates PostgreSQL SHOW INDEX Action.
+// This is an Action that can run `SHOW INDEX` command on PostgreSQL service with given DSN.
+func NewPostgreSQLShowIndexAction(id string, params *agentpb.StartActionRequest_PostgreSQLShowIndexParams) Action {
+	return &postgresqlShowIndexAction{
+		id:     id,
+		params: params,
+	}
+}
+
+// ID returns an Action ID.
+func (e *postgresqlShowIndexAction) ID() string {
+	return e.id
+}
+
+// Type returns an Action type.
+func (e *postgresqlShowIndexAction) Type() string {
+	return "postgresql-show-index"
+}
+
+// Run runs an Action and returns output and error.
+func (e *postgresqlShowIndexAction) Run(ctx context.Context) ([]byte, error) {
+	db, err := sql.Open("postgres", e.params.Dsn)
+	if err != nil {
+		return nil, err
+	}
+	defer db.Close() //nolint:errcheck
+
+	// TODO: Throw error if table doesn't exist.
+	rows, err := db.QueryContext(ctx, "SELECT * FROM pg_indexes WHERE tablename = $1", e.params.Table)
+	if err != nil {
+		return nil, err
+	}
+
+	columns, dataRows, err := readRows(rows)
+	if err != nil {
+		return nil, err
+	}
+	return jsonRows(columns, dataRows)
+}
+
+func (e *postgresqlShowIndexAction) sealed() {}
