c0689400037394adaa9adf3b9dac3802a4719ff7
Nurlan Moldomurov <nurlan.moldomurov@percona.com>
2020-03-26 18:14:22 +0300

    PMM-4192 Fix tests.

--- actions/mysql_show_create_table_action_test.go
+++ actions/mysql_show_create_table_action_test.go
@@ -85,11 +85,11 @@ CREATE TABLE "city" (
 			// `DEFAULT '0'` for Population
 			expected = strings.TrimSpace(`
 CREATE TABLE "city" (
-  "ID" int(11) NOT NULL AUTO_INCREMENT,
+  "ID" int NOT NULL AUTO_INCREMENT,
   "Name" char(35) NOT NULL DEFAULT '',
   "CountryCode" char(3) NOT NULL DEFAULT '',
   "District" char(20) NOT NULL DEFAULT '',
-  "Population" int(11) NOT NULL DEFAULT '0',
+  "Population" int NOT NULL DEFAULT '0',
   PRIMARY KEY ("ID"),
   KEY "CountryCode" ("CountryCode"),
   CONSTRAINT "city_ibfk_1" FOREIGN KEY ("CountryCode") REFERENCES "country" ("Code")
