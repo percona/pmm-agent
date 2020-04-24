4d004c7a7aeae85eb0bb41d66ee3942d0f3cccf8
Alexey Palazhchenko <alexey.palazhchenko@percona.com>
2020-04-24 16:41:28 +0300

    SAAS-20 Tweak tests.

--- actions/mysql_query_show_action_test.go
+++ actions/mysql_query_show_action_test.go
@@ -47,13 +47,13 @@ func TestMySQLQueryShow(t *testing.T) {
 		b, err := a.Run(ctx)
 		require.NoError(t, err)
 		assert.LessOrEqual(t, 16345, len(b))
-		assert.LessOrEqual(t, len(b), 21942)
+		assert.LessOrEqual(t, len(b), 24321)
 
 		data, err := agentpb.UnmarshalActionQueryResult(b)
 		require.NoError(t, err)
 		t.Log(spew.Sdump(data))
 		assert.LessOrEqual(t, 456, len(data))
-		assert.LessOrEqual(t, len(data), 589)
+		assert.LessOrEqual(t, len(data), 655)
 
 		var found int
 		for _, m := range data {
