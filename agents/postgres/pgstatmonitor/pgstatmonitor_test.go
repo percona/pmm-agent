5ec98583ec89e08c6773a2137bbb953303fa3421
Nurlan Moldomurov <nurlan.moldomurov@percona.com>
2020-10-12 23:09:37 +0300

    Skip tests until PG-139 is fixed.

--- agents/postgres/pgstatmonitor/pgstatmonitor_test.go
+++ agents/postgres/pgstatmonitor/pgstatmonitor_test.go
@@ -88,7 +88,6 @@ func filter(mb []*agentpb.MetricsBucket) []*agentpb.MetricsBucket {
 }
 
 func TestPGStatMonitorSchema(t *testing.T) {
-	t.Skip("Skip it until https://jira.percona.com/browse/PG-139 is fixed")
 	sqlDB := tests.OpenTestPostgreSQL(t)
 	defer sqlDB.Close() //nolint:errcheck
 	db := reform.NewDB(sqlDB, postgresql.Dialect, reform.NewPrintfLogger(t.Logf))
@@ -359,6 +358,7 @@ func TestPGStatMonitorSchema(t *testing.T) {
 	})
 
 	t.Run("CheckMBlkReadTime", func(t *testing.T) {
+		t.Skip("Skip it until https://jira.percona.com/browse/PG-139 is fixed")
 		r := rand.New(rand.NewSource(time.Now().Unix())) // nolint:gosec
 		tableName := fmt.Sprintf("customer%d", r.Int())
 		_, err := db.Exec(fmt.Sprintf(`
@@ -391,10 +391,6 @@ func TestPGStatMonitorSchema(t *testing.T) {
 
 		var buckets []*agentpb.MetricsBucket
 		for i := 0; i < 100; i++ {
-			rows, err := db.SelectAllFrom(pgStatMonitorView, "WHERE queryid IS NOT NULL AND query IS NOT NULL")
-			require.NoError(t, err)
-			tests.LogTable(t, rows)
-
 			buckets, err = m.getNewBuckets(context.Background(), 60)
 			require.NoError(t, err)
 			buckets = filter(buckets)
