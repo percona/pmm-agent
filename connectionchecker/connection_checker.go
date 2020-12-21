c655835524a7153b3f1b4e2b25d0d469266b8258
Maksym Hilliaka <maksym.hilliaka@percona.com>
2020-12-21 18:04:21 +0200

    PMM-6096 Fix govet issues

--- connectionchecker/connection_checker.go
+++ connectionchecker/connection_checker.go
@@ -137,7 +137,7 @@ func (cc *ConnectionChecker) checkMongoDBConnection(ctx context.Context, dsn str
 		return &res
 	}
 
-	resp := client.Database("admin").RunCommand(ctx, bson.D{{"listDatabases", 1}})
+	resp := client.Database("admin").RunCommand(ctx, bson.D{{Key: "listDatabases", Value: 1}})
 	if err = resp.Err(); err != nil {
 		cc.l.Debugf("checkMongoDBConnection: failed to runCommand listDatabases: %s", err)
 		res.Error = err.Error()
