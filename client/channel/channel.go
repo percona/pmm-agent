6c9dd1d49128ebdd99c99b2e701bf4ac138cb232
Jan Prukner <jan.prukner.sde@gmail.com>
2021-05-12 13:48:56 +0200

    move the import to the same place

--- client/channel/channel.go
+++ client/channel/channel.go
@@ -21,14 +21,13 @@ import (
 	"sync/atomic"
 
 	"github.com/golang/protobuf/proto" //nolint:staticcheck
+	"github.com/percona/pmm/api/agentpb"
 	"github.com/pkg/errors"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/sirupsen/logrus"
 	protostatus "google.golang.org/genproto/googleapis/rpc/status"
 	"google.golang.org/grpc/codes"
 	grpcstatus "google.golang.org/grpc/status"
-
-	"github.com/percona/pmm/api/agentpb"
 )
 
 const (
