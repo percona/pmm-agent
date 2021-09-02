524547b5b9d8d490985ccabd0a9d6edd721ceee4
Artem Gavrilov <artem.gavrilov@percona.com>
2021-09-02 16:09:02 +0200

    PMM-8269 Fix tests

--- connectionchecker/connection_checker_test.go
+++ connectionchecker/connection_checker_test.go
@@ -172,7 +172,7 @@ func TestConnectionChecker(t *testing.T) {
 				Type:    inventorypb.ServiceType_PROXYSQL_SERVICE,
 				Timeout: ptypes.DurationProto(time.Nanosecond),
 			},
-			expectedErr: `context deadline exceeded`,
+			expectedErr: `server selection error: context deadline exceeded, current topology: { Type: Unknown, Servers: [{ Addr: 127.0.0.1:27017, Type: Unknown }, ] }`,
 		},
 	}
 
