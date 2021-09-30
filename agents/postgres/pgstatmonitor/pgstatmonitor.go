c807ad613dda03f42e233811c049a6be5f5f7333
Jiri Ctvrtka <jiri.ctvrtka.sde@gmail.com>
2021-09-30 12:30:04 +0200

    PMM-8704 Mod. Fixies.

--- agents/postgres/pgstatmonitor/pgstatmonitor.go
+++ agents/postgres/pgstatmonitor/pgstatmonitor.go
@@ -264,7 +264,7 @@ func (m *PGStatMonitorQAN) makeBuckets(current, cache map[time.Time]map[string]*
 
 			mb.Postgresql.TopQueryid = currentPSM.TopQueryID
 			mb.Postgresql.ApplicationName = currentPSM.ApplicationName
-			mb.Postgresql.PlanId = currentPSM.QueryID
+			mb.Postgresql.Planid = currentPSM.PlanID
 
 			if (currentPSM.PlanTotalTime - prevPSM.PlanTotalTime) != 0 {
 				mb.Postgresql.MPlanTimeSum = float32(currentPSM.PlanTotalTime-prevPSM.PlanTotalTime) / 1000
