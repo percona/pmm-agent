75d111881dade838fa77d3e9092bb7a430242b38
Jiri Ctvrtka <jiri.ctvrtka.sde@gmail.com>
2021-10-20 15:51:20 +0200

    PMM-8588 Add histogram.

--- agents/postgres/pgstatmonitor/pgstatmonitor.go
+++ agents/postgres/pgstatmonitor/pgstatmonitor.go
@@ -267,6 +267,7 @@ func (m *PGStatMonitorQAN) makeBuckets(current, cache map[time.Time]map[string]*
 			mb.Postgresql.ApplicationName = currentPSM.ApplicationName
 			mb.Postgresql.Planid = currentPSM.PlanID
 			mb.Postgresql.QueryPlan = currentPSM.QueryPlan
+			mb.Postgresql.RespCalls = currentPSM.RespCalls
 
 			if (currentPSM.PlanTotalTime - prevPSM.PlanTotalTime) != 0 {
 				mb.Postgresql.MPlanTimeSum = float32(currentPSM.PlanTotalTime-prevPSM.PlanTotalTime) / 1000
