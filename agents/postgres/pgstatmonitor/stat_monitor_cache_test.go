1639d8e65401b7fefaf8eef025e1c810d150bd60
Nurlan Moldomurov <nurlan.moldomurov@percona.com>
2021-11-24 21:57:22 +0300

    PMM-8557 Fix tests and checks.

--- agents/postgres/pgstatmonitor/stat_monitor_cache_test.go
+++ agents/postgres/pgstatmonitor/stat_monitor_cache_test.go
@@ -1,3 +1,18 @@
+// pmm-agent
+// Copyright 2019 Percona LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package pgstatmonitor
 
 import (
@@ -20,15 +35,15 @@ func TestPGStatMonitorStructs(t *testing.T) {
 	_, err := db.Exec("CREATE EXTENSION IF NOT EXISTS pg_stat_monitor SCHEMA public")
 	assert.NoError(t, err)
 
-	defer func() {
-		_, err = db.Exec("DROP EXTENSION pg_stat_monitor")
-		assert.NoError(t, err)
-	}()
-
 	engineVersion := tests.PostgreSQLVersion(t, sqlDB)
 	if !supportedVersion(engineVersion) || !extensionExists(db) {
 		t.Skip()
 	}
+
+	defer func() {
+		_, err = db.Exec("DROP EXTENSION pg_stat_monitor")
+		assert.NoError(t, err)
+	}()
 
 	m := setup(t, db, false)
 	current, cache, err := m.monitorCache.getStatMonitorExtended(context.TODO(), db.Querier, m.pgsmNormalizedQuery)
