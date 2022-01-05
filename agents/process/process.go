0ad3841ae16a0556d5ce936879b34f4c306775b9
Jiri Ctvrtka <jiri.ctvrtka.sde@gmail.com>
2022-01-05 13:11:41 +0100

    PMM-8141 Log missed error.

--- agents/process/process.go
+++ agents/process/process.go
@@ -181,7 +181,10 @@ func (p *Process) toWaiting() {
 	case <-t.C:
 		// VM_AGENT need recreate config file in temp dir
 		if p.params.Type == inventorypb.AgentType_VM_AGENT {
-			p.params.TemplateRenderer.RenderFiles(p.params.TemplateParams)
+			_, err := p.params.TemplateRenderer.RenderFiles(p.params.TemplateParams)
+			if err != nil {
+				p.l.Warnf("Process: failed to regenerate config in %s.", p.params.TemplateRenderer.TempDir)
+			}
 		}
 
 		go p.toStarting()
