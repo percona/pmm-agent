fce92b60a0e0e876d7ea032192720b5c17d235ae
Przemyslaw Kadej <przemyslaw.kadej@implex.dev>
2022-04-20 00:32:13 +0200

    PMM-4879 Linter & tests fix.

--- defaultsfile/defaultsfile.go
+++ defaultsfile/defaultsfile.go
@@ -13,29 +13,35 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// Package defaultsfile provides managing of defaults file.
 package defaultsfile
 
 import (
 	"fmt"
-	"github.com/percona/pmm/api/agentpb"
-	"github.com/percona/pmm/api/inventorypb"
 	"os/user"
 	"path/filepath"
 	"strings"
 
+	"github.com/percona/pmm/api/agentpb"
+	"github.com/percona/pmm/api/inventorypb"
 	"github.com/pkg/errors"
 	"gopkg.in/ini.v1"
 )
 
+// DefaultsFile is a struct with database specs fetched from file.
 type DefaultsFile struct {
 	username string
 	password string
+	host     string
+	port     uint32
 }
 
+// New creates new DefaultsFile.
 func New() *DefaultsFile {
 	return &DefaultsFile{}
 }
 
+// ParseDefaultsFile parses given defaultsFile. It returns the database specs.
 func (d *DefaultsFile) ParseDefaultsFile(req *agentpb.ParseDefaultsFileRequest) *agentpb.ParseDefaultsFileResponse {
 	var res agentpb.ParseDefaultsFileResponse
 	defaultsFile, err := parseDefaultsFile(req.ConfigPath, req.ServiceType)
@@ -46,6 +52,8 @@ func (d *DefaultsFile) ParseDefaultsFile(req *agentpb.ParseDefaultsFileRequest) 
 
 	res.Username = defaultsFile.username
 	res.Password = defaultsFile.password
+	res.Host = defaultsFile.host
+	res.Port = defaultsFile.port
 
 	return &res
 }
@@ -57,27 +65,36 @@ func parseDefaultsFile(configPath string, serviceType inventorypb.ServiceType) (
 
 	switch serviceType {
 	case inventorypb.ServiceType_MYSQL_SERVICE:
-		return parseMySqlDefaultsFile(configPath)
-	default:
+		return parseMySQLDefaultsFile(configPath)
+	case inventorypb.ServiceType_EXTERNAL_SERVICE:
+	case inventorypb.ServiceType_HAPROXY_SERVICE:
+	case inventorypb.ServiceType_MONGODB_SERVICE:
+	case inventorypb.ServiceType_POSTGRESQL_SERVICE:
+	case inventorypb.ServiceType_PROXYSQL_SERVICE:
+	case inventorypb.ServiceType_SERVICE_TYPE_INVALID:
 		return nil, errors.Errorf("unimplemented service type %s", serviceType)
 	}
 
+	return nil, errors.Errorf("unimplemented service type %s", serviceType)
 }
 
-func parseMySqlDefaultsFile(configPath string) (*DefaultsFile, error) {
+func parseMySQLDefaultsFile(configPath string) (*DefaultsFile, error) {
 	configPath, err := expandPath(configPath)
 	if err != nil {
-		return nil, fmt.Errorf("fail to normalize path: %v", err)
+		return nil, fmt.Errorf("fail to normalize path: %w", err)
 	}
 
 	cfg, err := ini.Load(configPath)
 	if err != nil {
-		return nil, fmt.Errorf("fail to read config file: %v", err)
+		return nil, fmt.Errorf("fail to read config file: %w", err)
 	}
 
+	port, _ := cfg.Section("client").Key("port").Uint()
 	return &DefaultsFile{
 		username: cfg.Section("client").Key("user").String(),
 		password: cfg.Section("client").Key("password").String(),
+		host:     cfg.Section("client").Key("host").String(),
+		port:     uint32(port),
 	}, nil
 }
 
@@ -85,7 +102,7 @@ func expandPath(path string) (string, error) {
 	if strings.HasPrefix(path, "~/") {
 		usr, err := user.Current()
 		if err != nil {
-			return "", err
+			return "", fmt.Errorf("failed to expand path: %w", err)
 		}
 		return filepath.Join(usr.HomeDir, path[2:]), nil
 	}
