1cc70c579a619bb0f3a36265b2a545d0338269f0
Alexey Mukas <ritbl@protonmail.com>
2022-05-04 11:33:20 +0200

    style

--- mongo_fix/mongo_fix.go
+++ mongo_fix/mongo_fix.go
@@ -24,14 +24,14 @@ import (
 
 // ClientOptionsForDSN applies URI to Client.
 func ClientOptionsForDSN(dsn string) (*options.ClientOptions, error) {
+	clientOptions := options.Client().ApplyURI(dsn)
+
+	// Workaround for PMM-9320
 	parsedDsn, err := url.Parse(dsn)
 	if err != nil {
 		return nil, errors.Wrap(err, "cannot parse DSN")
 	}
 
-	clientOptions := options.Client().ApplyURI(dsn)
-
-	// Workaround for PMM-9320
 	username := parsedDsn.User.Username()
 	password, _ := parsedDsn.User.Password()
 	if username != "" || password != "" {
